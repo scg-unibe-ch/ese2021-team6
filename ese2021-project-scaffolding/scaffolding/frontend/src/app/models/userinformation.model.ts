ba95e660b7e0ba01ca77b41d92920e30d8157c61
LamaCharge <90970283+LamaCharge@users.noreply.github.com>
2021-10-12 18:47:57 +0200

    Added Registerfields Frontend
    Extended all the Registerfields on the Frontend(FE) for the Fan. Added the userinformation module.

---
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/models/userinformation.model.ts
@@ -0,0 +1,13 @@
+export class Userinformation {
+
+  constructor(
+    public firstname: string,
+    public lastname: string,
+    public email: string,
+    public address: string,
+    public zipCode: number,
+    public city: string,
+    public birthday: number,
+    public phonenumber: number
+  ) {}
+}
