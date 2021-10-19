0aa8321d92ab7a822f74d407d0f492e4f0a9ab30
roman1489-afk <martinezz99999@gmail.com>
2021-10-19 23:29:16 +0200

    restore
    restore

--- ese2021-project-scaffolding/scaffolding/backend/src/middlewares/checkRegist.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/middlewares/checkRegist.ts
@@ -4,36 +4,42 @@ import { UserAttributes, User } from '../models/user.model';
 
 // this way you can just define a function and export it instead of a whole class
 export function checkRegist(req: Request, res: Response, next: any) {
+   // try {
 
         let sameName = false;
         let sameMail = false;
+<<<<<<< Updated upstream
         // since the authorization header consists of "Bearer <token>" where <token> is a JWT token
+=======
+
+>>>>>>> Stashed changes
         const desiredUserName = req.body.userName;
         const desiredEmail = req.body.email;
-        console.log(desiredUserName);
-        console.log(desiredEmail);
-        // console.log(User.findOne({where: {userName: desiredUserName}}));
+
 
         User.findAndCountAll({where: {userName: desiredUserName}}).then(result => {
-            console.log('beforIf');
-            console.log(result.count);
             if (result.count > 1) {
                     sameName = true;
-                console.log(sameName);
             }
         });
 
-        // User.findOne(({where: {userName: desiredUserName}})).then({
-        //        res.status(403).send({ message: 'Username already in use.' }));
-        console.log(sameName);
-        if (sameName) {
-            console.log('yep');
-            res.status(403).send({ message: 'Username already in use.' });
-        }
-     //   if (User.findOne({where: {email: desiredEmail}})) {
-      //      res.status(403).send({ message: 'E-mail already in use.' });
-      //  }
-        // adds the field "tokenPayload" to the request enabling following functions to use data from the token
+        User.findAndCountAll({where: {email: desiredEmail}}).then(result => {
+            if (result.count > 1) {
+                sameMail = true;
+            }
+        });
 
-        next();
+        setTimeout(() => {if (sameName) {
+            res.status(403).send({ message: 'Username already in use.' }); }}, 2000);
+
+        setTimeout(() => {if (sameMail) {
+            res.status(403).send({ message: 'Email already in use.' }); }}, 2000);
+
+        setTimeout( () => {
+            next();
+        }, 5000);
+
+    // } catch (err) {
+   //    res.status(403).send({ message: 'Username or Email already in use.' });
+    // }
 }
