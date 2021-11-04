8ad414e3085d81ec8bfe845a99ea90d5a8793dfe
LamaCharge <90970283+LamaCharge@users.noreply.github.com>
2021-11-05 00:08:57 +0100

    Added basic documentation
    Added basic documentation and reacted to some of the feedback of Marco

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post/post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post/post.component.ts
@@ -12,7 +12,7 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-
+  //Event Emitters to interact with Backend
   @Input()
   post: Post = new Post(0, '', '', 0, 0, 0, 0, []);
 
@@ -55,7 +55,7 @@ export class PostComponent {
   downVote(): void {
     this.downvoteEvent.emit(this.post);
   }
-
+  // Opens the popup to write a comment to the post
   openPopUp(): void {
     const dialogRef = this.dialog.open(DialogComponent, {
       width: '750px',
@@ -63,11 +63,12 @@ export class PostComponent {
       data: { value: "comment" },
     });
      const commentEvent = dialogRef.componentInstance.createComment.subscribe(result => {
-       console.log("EVENT RECEIVED")
+      // console.log("EVENT RECEIVED")
       this.createComment(result);
     })
   }
 
+// CREATE - Comment
   createComment(text: string): void {
     console.log(text)
     this.httpClient.post(environment.endpointURL + "comment", {
@@ -80,6 +81,14 @@ export class PostComponent {
     }).subscribe((item: any) => {
       this.post.comments.push(new Comment(item.commentId, item.text, item.upvoteCount 
         ,item.downvoteCount, item.postId, item.userId));
+    });
+  }
+
+// DELETE - Comment
+  deleteComment(comment: Comment): void {
+    console.log(comment.commentId)
+     this.httpClient.delete(environment.endpointURL + "comment/" + comment.commentId).subscribe(() => {
+      this.post.comments.splice(this.post.comments.indexOf(comment), 1);
     });
   }
 
